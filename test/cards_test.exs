f6cd96c2d7872055fa683ccfd344d40929c56a16
leonel Mendez <Leo Mendez>
2017-01-31 10:39:59 -0600

    NEW: Added tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,14 +2,13 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "Contains card in deck" do
-    deck = ["Ace","Two","Three"]
-    assert Cards.contains?(deck,"Ace")
+  test "create deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
   end
 
-  test "Not Contains card in deck" do
-    deck = ["Ace","Two","Three"]    
-    assert Cards.contains?(deck,"Four")
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
   end
-  
 end
